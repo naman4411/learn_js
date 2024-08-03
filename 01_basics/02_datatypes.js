41e013ff3c334548ca3cd4843f8355ecbba0e340
Naman V <namanvishvakarma@gmail.com>
2024-08-03 16:04:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//alert(3+3)
+console.log(12/3)
+
+let name = "naman"
+let age  = 18
+let IsloggedIn= false
+let state = null
+
+//number=> 2 to power 53
+//bigint =>
+//string => ""
+// boolean=> true/false
+//null=> standalone  value
+//undefined=>
+//symbol=> unique
+
+//object=>
+
+console.log(typeof null) => //object
