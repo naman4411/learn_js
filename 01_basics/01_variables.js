5d4906db64dfd1728a128e06d5f9e5c8f90a7cf9
Naman V <namanvishvakarma@gmail.com>
2024-08-05 17:52:00 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,12 @@
-const accountId = 144533;
+const accountId = "144533";
 let accountEmail = "naman@gmail.com";
 var accountpassword = "12345";
-accountCity = "jaipur";
+accountcity = "jaipur";
 
 accountEmail = "hcbc@gmail.com";
 accountpassword = "22222";
-accountCity = bangalore;
+accountcity = "bangalore";
+
 console.log(accountId);
 
-console.table([accountId, accountEmail])
+console.table([accountId, accountEmail, accountpassword, accountcity])
