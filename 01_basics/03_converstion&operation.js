acb3ca2dbe6835bd17f0adfb2d3453b66ada9e42
Naman V <namanvishvakarma@gmail.com>
2024-08-10 21:24:11 +0530

    Update 03_converstion&operation.js

--- 01_basics/03_converstion&operation.js
+++ 01_basics/03_converstion&operation.js
@@ -1,2 +1,30 @@
 
 
+let value = -3
+let negvalue = -value
+console.log(negvalue)
+    //console.log(2*2)
+    //console.log(2**4)
+    //console.log(2/3)
+    //console.log(2%3)
+
+let str1 = " hello"
+let str2 = " naman"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+
+//js trets whole code as trets first one
+
+console.log("1"+2) //output=>12
+console.log(1+"2") //output=>12
+console.log("1"+2+2) //output=>122
+console.log(1+2+"2") //output=>32
+
+console.log((3+4)*5%3) //output=>2 
+console.log(true) //output=> true
+console.log(+true) //output=> 1
+
+
