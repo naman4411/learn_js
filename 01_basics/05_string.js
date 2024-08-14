951174715d7b0090ac57d2581855a6dad3ac1fb6
Naman V <namanvishvakarma@gmail.com>
2024-08-14 22:40:06 +0530

    Create 05_string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name = "Naman"
+const repoCount = 50
+    // use `` and ${} to print object refrence in output
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String(`namanhh`)
+    // make sure brackets are *()* are apply after function =>toUpperCase()
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(2));
+
+// indexoff se patat chlega konsa char kis position p hai
+
