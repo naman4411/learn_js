de69333b423d159e747f9aaa8e151d66e2778044
Naman V <namanvishvakarma@gmail.com>
2024-09-02 11:37:10 +0530

    arrays.js

--- 01_arrays.js
+++ 01_arrays.js
@@ -1,4 +1,4 @@
-// const MyArry= [0,1,2,3,4]
+// const MyArry= [0,1,2,3,4,5]
 const Myheros = ["shaktiman", "ironman"]
 
 const myarr2 = new Array(0, 1, 2, 3, 4)
@@ -6,7 +6,33 @@ const myarr2 = new Array(0, 1, 2, 3, 4)
 console.log(myarr2[0]);
 
 // Array methods
-const MyArry = [0, 1, 2, 3, 4]
-    // MyArry.push(6)
-MyArry.pop()
+// MyArry.push(6)
+// MyArry.pop()
+// MyArry.unshift(9)
+MyArry.shift()
 console.log(MyArry);
+
+// const MyArry = [0, 1, 2, 3, 4,5]
+// const newArry = MyArry.join () //join can convert type of arry to string 
+// console.log(typeof MyArry)
+
+// slice,spice
+ const MyArry = [0, 1, 2, 3, 4, 5]
+ console.log("A", MyArry);
+ const myn1= MyArry.slice(1,3)
+ 
+ console.log(myn1);
+ 
+// splice
+  const MyArr = [0, 1, 2, 3, 4, 5]
+ console.log("A", MyArr);
+ const myn2= MyArr.splice(1,3)
+ 
+ console.log(myn2);
+
+
+
+
+
+
+
