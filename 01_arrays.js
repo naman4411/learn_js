6dc802a9de6c01f8c4cc6e77253da0b555bf1014
Naman V <namanvishvakarma@gmail.com>
2024-09-02 11:36:08 +0530

    Add files via upload

---
+++ 01_arrays.js
@@ -0,0 +1,12 @@
+// const MyArry= [0,1,2,3,4]
+const Myheros = ["shaktiman", "ironman"]
+
+const myarr2 = new Array(0, 1, 2, 3, 4)
+
+console.log(myarr2[0]);
+
+// Array methods
+const MyArry = [0, 1, 2, 3, 4]
+    // MyArry.push(6)
+MyArry.pop()
+console.log(MyArry);
