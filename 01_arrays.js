d8d84d9c4ff4ecb1e9de1aad2d5eaf28af5e2cb4
Naman V <namanvishvakarma@gmail.com>
2024-09-05 11:08:03 +0530

    Update 01_arrays.js

--- 01_arrays.js
+++ 01_arrays.js
@@ -17,22 +17,35 @@ console.log(MyArry);
 // console.log(typeof MyArry)
 
 // slice,spice
- const MyArry = [0, 1, 2, 3, 4, 5]
- console.log("A", MyArry);
- const myn1= MyArry.slice(1,3)
- 
- console.log(myn1);
- 
+const MyArry = [0, 1, 2, 3, 4, 5]
+console.log("A", MyArry);
+const myn1 = MyArry.slice(1, 3)
+
+console.log(myn1);
+
 // splice
-  const MyArr = [0, 1, 2, 3, 4, 5]
- console.log("A", MyArr);
- const myn2= MyArr.splice(1,3)
- 
- console.log(myn2);
+const MyArr = [0, 1, 2, 3, 4, 5]
+console.log("A", MyArr);
+const myn2 = MyArr.splice(1, 3)
 
+console.log(myn2);
 
+// const Marvel_heros = ["ironman", "Thor", "hawkeye"]
+// const dc_heros = ["superman", "batman", "Wonderwomen"]
 
+// Marvel_heros.push(dc_heros)
+// console.log(Marvel_heros)
+// const all_hereos = Marvel_heros.concat(dc_heros) => you can add on one array at a time where spread can add multiple arrays
 
+// console.log(all_hereos[4])
 
+// console.log(0.1+0.2==0.3)
 
+const Marvel_heros = ["ironman", "Thor", "hawkeye"]
+const dc_heros = ["superman", "batman", "Wonderwomen"]
 
+const all_heros = [...Marvel_heros,...dc_heros]
+console.log(all_heros)
+
+console.log(Array.isArray("naman"))
+console.log(Array.from({name:"naman"})) //=>intresting
