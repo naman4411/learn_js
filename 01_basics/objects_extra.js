7bca44ee66bf54d7c4f63e08179baaa7155c86e3
Naman V <namanvishvakarma@gmail.com>
2024-09-11 16:12:19 +0000

    obj_extra

---
+++ 01_basics/objects_extra.js
@@ -0,0 +1,26 @@
+// const tinderUser = {}
+
+// tinderUser.id = "123abc",
+// tinderUser.name = "vivek",
+// tinderUser.isLoggedIn = false
+// // console.log(tinderUser);
+
+// const regularUser ={
+// email: "some@gmail.com",
+// fullname:{
+//     userfullname:{
+//         firstname:"naman",
+//         Lastname : "vishvakarma"
+//      }
+// }
+// }
+// console.log( regularUser.fullname)
+
+// ****************Adding Two Objects like array********************
+const obj1= {1:"a", 2:"b"}
+const obj2= {3:"a", 4:"b"}
+
+const obj3= Object.assign( {},obj1,obj2)
+console.log(obj3) ;
+
+
