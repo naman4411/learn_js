b362a8a8ef95d74a84bd153d8ce5c53857ecc813
Naman V <namanvishvakarma@gmail.com>
2024-09-11 15:26:05 +0000

    Obejects.less

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -1,4 +1,41 @@
 // literals in object
 
+const mySym = Symbol("key1")
 
-console.log(1+2)
+const JsUser = {
+name: "Naman",
+[mySym] : "mykey1",
+age: 20 ,
+Location: "Lucnknow",
+email: "naman@salesforce.com", 
+isLoggedIn:false,
+lastLoginIn: ["monday", "tuesday"]
+}
+
+// **************we cAN access obj by two metods*********
+
+// console.log(JsUser.email) 
+// console.log(JsUser[mySym]) <= for sym don't use semicolon to access 
+
+// how to freez an obj
+JsUser.email = "naman@chatgptcom"
+// Object.freeze(JsUser)
+JsUser.email = "naman@nvidia.com"
+// console.log(JsUser)
+
+// ***************Function access by refernce**********
+
+JsUser.greeting = function(){
+    console.log("hello world")
+}
+
+
+// ***************Function access by objects**********
+
+JsUser.greetingTwo = function(){
+    console.log(`hello world,${this.name}`);
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
+
