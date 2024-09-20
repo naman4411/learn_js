bc18a7ba77583482f42952438a4bdf700c87b1ea
Naman V <namanvishvakarma@gmail.com>
2024-09-20 05:00:09 +0000

    something about scope

---
+++ 02_basics/02_scope.js
@@ -0,0 +1,37 @@
+// let a =300 
+// if (true) {
+//      let a =10
+//      const b=20
+//     //  console.log("innner:," ,a);
+// }
+// // console.log("result:",a)
+
+// function one(){
+//     const username ="naman"
+
+//    function two(){
+//     const website ="youtube"
+//     // console.log(username);
+// }  
+// // console.log(website)
+// two()
+// }
+// one()
+
+// if(true){
+//     const username ="naman"
+//     if(username==="naman"){
+//         const website=" youtube"
+//         // console.log(username+website)
+//     }
+// }
+// ****************intresting**********************
+// function addone(num){
+//     return num + 1
+// }
+// console.log(addone(5))
+
+const addone = function(num){
+    return num +2
+}
+console.log(addone(6))
